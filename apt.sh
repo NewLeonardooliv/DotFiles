2d30122a79cab204d50feb2100b30d0d79ade034
Leonardo Oliveira <Leonardo_oliveira7@hotmail.com>
2022-04-30 09:08:34 -0300

    DotFiles

--- apt.sh
+++ apt.sh
@@ -2,32 +2,13 @@
 
 PROGRAMS=(
   curl
-  plank
-  git
   redis-server
   apache2
   mysql-server
   php 
   postgresql
   postgresql-contrib
-  libapache2-mod-php 
-  php-mysql
-  php-mcrypt
-  php-zip
-  php-gettext
-  php-common
-  php-xml
-  php-dom
-  php-pgsql
-  php-mbstring
-  php-curl
-  php-gd
-  php-dev
   memcached
-  php-memcached
-   php-sybase
-  php-json
-  php-redis
 )
 
 sudo apt update -y
